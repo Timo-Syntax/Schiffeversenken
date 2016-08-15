4476404dba4f12ba8eb28b9def47598b9015bca8
Lukas Bartl <lukibartl@gmail.com>
2016-08-15 14:15:39 +0200

    Lizenz hinzugefügt und an SPIELFELD weitergearbeitet

---
+++ README.md
@@ -0,0 +1,9 @@
+# Schiffeversenken
+
+Copyright (C) 2016 Dominik Huber, Lukas Bartl
+Dieses Programm ist freie Software. Es darf verändert und weitergegeben werden.
+Es gibt keinerlei Garantien.
+Lizenz GPLv3+: GNU GPL Version 3 oder höher (siehe COPYING)
+
+Verwendet [Alpha Engine](http://engine-alpha.org/) unter den Bedingungen der GPLv3+:
+[Lizenz](http://engine-alpha.org/wiki/Lizenzen)
--- SPIELFELD.java
+++ SPIELFELD.java
@@ -1,34 +1,73 @@
+/* Copyright (C) 2016 Dominik Huber, Lukas Bartl
+ * Diese Datei ist ein Teil von Schiffeversenken.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 public class SPIELFELD extends SPIEL {
 
-    private SCHIFF[]spielfeld1;
-    private SCHIFF[]spielfeld2;
+    private SCHIFF[][] spielfeld1, spielfeld2, spielfeldAktuell;
+    private int xAktuell, yAktuell;
 
     public SPIELFELD(){
         super();
-        spielfeld1 = new SCHIFF[100];
-        int x = 0;
-        for(int z1=0; z1<10; z1++){
-            for(int z2=0; z2<10; z2++){
-                spielfeld1[x] = new SCHIFF(30+z2*36+12+10, 125+z1*36+12);
+        spielfeld1 = new SCHIFF[10][10];
+
+        for(int x=0; x<10; x++){
+            for(int y=0; y<10; y++){
+                spielfeld1[x][y] = new SCHIFF(20+x*36+12+10, 125+y*36+12);
             }
         }
-        
-        
-        
-        spielfeld2 = new SCHIFF[100];
-        x=0;
-        for(int z1=0; z1<10; z1++){
-            for(int z2=0; z2<10; z2++){
-                spielfeld2[x] = new SCHIFF(30+360+30+z2*36+12+20, 125+z1*36+12);
+
+        spielfeld2 = new SCHIFF[10][10];
+
+        for(int x=0; x<10; x++){
+            for(int y=0; y<10; y++){
+                spielfeld2[x][y] = new SCHIFF(20+360+30+x*36+12+20, 125+y*36+12);
             }
         }
+
+        spielfeldAktuell = spielfeld1;
+        xAktuell = 0;
+        yAktuell = 0;
+        spielfeldAktuell[xAktuell][yAktuell].setzeFarbe("rot");
     }
-    
+
     @Override
     public void tick() {}
-    
+
     @Override
     public void tasteReagieren( int code ) {
-        
+        if ( code == 26 && yAktuell > 0 ) { // oben
+            spielfeldAktuell[xAktuell][yAktuell].setzeFarbe("grau");
+            spielfeldAktuell[xAktuell][--yAktuell].setzeFarbe("rot");
+        } else if ( code == 27 && xAktuell < 9 ) { // rechts
+            spielfeldAktuell[xAktuell][yAktuell].setzeFarbe("grau");
+            spielfeldAktuell[++xAktuell][yAktuell].setzeFarbe("rot");
+        } else if ( code == 28 && yAktuell < 9 ) { // unten
+            spielfeldAktuell[xAktuell][yAktuell].setzeFarbe("grau");
+            spielfeldAktuell[xAktuell][++yAktuell].setzeFarbe("rot");
+        } else if ( code == 29 && xAktuell > 0 ) { // links
+            spielfeldAktuell[xAktuell][yAktuell].setzeFarbe("grau");
+            spielfeldAktuell[--xAktuell][yAktuell].setzeFarbe("rot");
+        } else if ( code == 31 ) { // Enter
+            spielfeldAktuell[xAktuell][yAktuell].setzeFarbe("grau");
+            
+            spielfeldAktuell = spielfeldAktuell == spielfeld1 ? spielfeld2 : spielfeld1; // Anderes Spielfeld
+            xAktuell = 0;
+            yAktuell = 0;
+            spielfeldAktuell[xAktuell][yAktuell].setzeFarbe("rot");
+        }
     }
 }
